a53c0945d76b42242a58028e8c8ba4430814e4c0
jadsongmatos <jadson.g-matos@outlook.com>
2023-02-02 22:27:23 -0300

    3 match simples

--- src/main.rs
+++ src/main.rs
@@ -40,64 +40,37 @@ fn cipher_xor(text: &str, b: u8) -> String {
 fn analyze_text(text: &String) -> f32 {
     let mut score = 0.0;
     let mut letters: f32 = 0.0;
-    for tex in text.chars() {
+    for c in text.chars() {
         letters = letters + 1.0;
-        match tex {
-            'a' => score = score + 8.167,
-            'A' => score = score + 8.167,
-            'b' => score = score + 1.492,
-            'B' => score = score + 1.492,
-            'c' => score = score + 2.782,
-            'C' => score = score + 2.782,
-            'd' => score = score + 4.253,
-            'D' => score = score + 4.253,
-            'e' => score = score + 12.702,
-            'E' => score = score + 12.702,
-            'f' => score = score + 2.228,
-            'F' => score = score + 2.228,
-            'g' => score = score + 2.015,
-            'G' => score = score + 2.015,
-            'h' => score = score + 6.094,
-            'H' => score = score + 6.094,
-            'i' => score = score + 6.966,
-            'I' => score = score + 6.966,
-            'j' => score = score + 0.153,
-            'J' => score = score + 0.153,
-            'k' => score = score + 0.772,
-            'K' => score = score + 0.772,
-            'l' => score = score + 4.025,
-            'L' => score = score + 4.025,
-            'm' => score = score + 2.406,
-            'M' => score = score + 2.406,
-            'n' => score = score + 6.749,
-            'N' => score = score + 6.749,
-            'o' => score = score + 7.507,
-            'O' => score = score + 7.507,
-            'p' => score = score + 1.929,
-            'P' => score = score + 1.929,
-            'q' => score = score + 0.095,
-            'Q' => score = score + 0.095,
-            'r' => score = score + 5.987,
-            'R' => score = score + 5.987,
-            's' => score = score + 6.327,
-            'S' => score = score + 6.327,
-            't' => score = score + 9.056,
-            'T' => score = score + 9.056,
-            'u' => score = score + 2.758,
-            'U' => score = score + 2.758,
-            'v' => score = score + 0.978,
-            'V' => score = score + 0.978,
-            'w' => score = score + 2.360,
-            'W' => score = score + 2.360,
-            'x' => score = score + 0.150,
-            'X' => score = score + 0.150,
-            'y' => score = score + 1.974,
-            'Y' => score = score + 1.974,
-            'z' => score = score + 0.074,
-            'Z' => score = score + 0.074,
-            ' ' => score = score + 1.0,
-            _ => {}
-        }
+        score += match c {
+            'a' | 'A' => 8.167,
+            'b' | 'B' => 1.492,
+            'c' | 'C' => 2.782,
+            'd' | 'D' => 4.253,
+            'e' | 'E' => 12.702,
+            'f' | 'F' => 2.228,
+            'g' | 'G' => 2.015,
+            'h' | 'H' => 6.094,
+            'i' | 'I' => 6.966,
+            'j' | 'J' => 0.153,
+            'k' | 'K' => 0.772,
+            'l' | 'L' => 4.025,
+            'm' | 'M' => 2.406,
+            'n' | 'N' => 6.749,
+            'o' | 'O' => 7.507,
+            'p' | 'P' => 1.929,
+            'q' | 'Q' => 0.095,
+            'r' | 'R' => 5.987,
+            's' | 'S' => 6.327,
+            't' | 'T' => 9.056,
+            'u' | 'U' => 2.758,
+            'v' | 'V' => 0.978,
+            'w' | 'W' => 2.360,
+            'x' | 'X' => 0.150,
+            'y' | 'Y' => 1.974,
+            'z' | 'Z' => 0.074,
+            _ => 0.0,
+        };
     }
 
     let chance = score / letters;
@@ -125,8 +98,11 @@ fn test_key(input: &str) -> TestKeys {
 
     for n in 0..255 {
         old.text = cipher_xor(input, n);
-        old.score = analyze_text(&old.text);
+        old.score = analyze_text(&old.text) ;
         println!("testkey {} {}", n, old.score);
+        if n == 88 {
+            println!("{} {:?} {}", n,old.text, old.score);
+        }
         if old.score >= best.score {
             best.text = old.text;
             best.score = old.score;
